db43474f07a5ffaeb58d5e881548988ec2a8b0fe
timkorba34 <timkorba@yahoo.com>
2026-05-13 16:53:58 -0400

    Update gap_assessment_agent.py

--- gap_assessment_agent.py
+++ gap_assessment_agent.py
@@ -939,7 +939,7 @@ Phase, Timeline, Strategic Objective, Key Activities, Expected Outcome, Business
             "analytics_environment_summary",
             "current_architecture_diagram",
             "s4_future_state_diagram",
-            "reporting_dependency_diagram"
+            "reporting_dependency_diagram",
             "current_system_inventory",
             "current_data_flow_summary",
             "reporting_dependency_map",
