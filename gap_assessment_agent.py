28804c82a58bd0928feef7d049ac18b212f0fe4a
timkorba34 <timkorba@yahoo.com>
2026-05-26 12:47:50 -0400

    Update gap_assessment_agent.py

--- gap_assessment_agent.py
+++ gap_assessment_agent.py
@@ -1437,7 +1437,7 @@ def build_docx(data, client_name, assessment_type):
     add_paragraph(doc, data.get("executive_summary_text", ""))
 
     add_heading(doc,"Assessment Confidence",1)
-        add_heading(doc, "Assessment Confidence", 1)
+    add_heading(doc, "Assessment Confidence", 1)
 
     confidence = data.get("assessment_confidence", 75)
     missing = data.get("missing_inputs", [])
